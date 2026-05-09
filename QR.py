5281c495bfe9cd9050371c4786bd2ad019653bf9
IamNotReal12 <carlosmario12345678yt@gmail.com>
2026-05-09 05:13:46 -0500

    Add files via upload

--- QR.py
+++ QR.py
@@ -1,6 +1,7 @@
 import customtkinter as kc
 import qrcode as QR
 from PIL import Image
+from CTkColorPicker import *
 
 kc.set_appearance_mode("dark")
 kc.set_default_color_theme("blue")
@@ -13,20 +14,39 @@ App.grid_columnconfigure((0, 1), weight=1)
 App.grid_rowconfigure(0, weight=1)
 
 
+def Color_fill():
+    Color_selection = AskColor()
+    color = Color_selection.get()
+
+    if color:
+        global qr_color_fill
+        qr_color_fill = color
+def Color_back():
+    Color_Back = AskColor()
+    Color_F_Back = Color_Back.get()
+    
+    
+    if Color_F_Back:
+        global Qr_color_Back
+        Qr_color_Back = Color_F_Back
+        
 def Theme(valor):
     if valor == "Dark":
         kc.set_appearance_mode("dark")
-        Subtitulo.configure(text_color="#AAAAAA")
+        Subtitulo.configure(text_color="#FCF4F4")
+        Caja_Frame.configure(fg_color="#333333")
+
     elif valor == "Light":
+
         kc.set_appearance_mode("light")
         Entry_convert.configure(text_color="#ffffff")
         Subtitulo.configure(text_color="#000000")
-        
+        Caja_Frame.configure(fg_color="#2c2a2a")
 
 
 def CrearQR():
     contenido = str(Entry_convert.get().strip())
-
+    global qr_color_fill,Qr_color_Back
     if not contenido:
         return
 
@@ -37,7 +57,7 @@ def CrearQR():
     qr.add_data(contenido)
     qr.make(fit=True)
 
-    img_pillow = qr.make_image(fill_color="#17d823", back_color="#ffffff").convert(
+    img_pillow = qr.make_image(fill_color=qr_color_fill, back_color=Qr_color_Back).convert(
         "RGB"
     )
 
@@ -73,7 +93,7 @@ Encabezado.grid(row=0, column=0, pady=(50, 10))
 Subtitulo = kc.CTkLabel(
     Contenedor,
     text="Convierte tus links o textos instantáneamente",
-    text_color="#AAAAAA",
+    text_color="#FCF4F4",
     font=kc.CTkFont(family="Century Gothic", size=15),
 )
 Subtitulo.grid(row=1, column=0, pady=(0, 40))
@@ -114,22 +134,76 @@ Caja_Frame.grid_propagate(False)
 label_Image = kc.CTkLabel(Caja_Frame, text="")
 label_Image.grid(row=0, column=0)
 
+Tab_color = kc.CTkTabview(
+    App, 
+    width=400, 
+    height=400, 
+    fg_color="#1A1A1A", 
+    segmented_button_selected_color="#6bde18",
+    segmented_button_selected_hover_color="#56b313",
+    segmented_button_unselected_color="#2b2b2b",
+    text_color="#ffffff",
+    corner_radius=25,
+    border_width=2,
+    border_color="#6bde18"
+)
+Tab_color.grid(row=0, column=1, padx=20, pady=20)
+
+Tab_color.add("Modo")
+Tab_color.add("Qrcolor")
+
+Tab_color.tab("Modo").grid_columnconfigure(0, weight=1)
 
 Colores = kc.CTkSegmentedButton(
-    App,
+    Tab_color.tab("Modo"),
     values=["Light", "Dark"],
-    fg_color="#1A1A1A",
+    fg_color="#2b2b2b",
     selected_color="#6bde18",
     selected_hover_color="#56b313",
-    text_color="#ffffff",
-    unselected_color="#AAAAAA",
-    width=180,
-    height=40,
-    border_width=2,
-    font=kc.CTkFont(family="Century Gothic", size=13, weight="bold"),
-    command=Theme
+    text_color="#000000",
+    unselected_color="#333333",
+    unselected_hover_color="#444444",
+    width=250,
+    height=50,
+    corner_radius=10,
+    font=kc.CTkFont(family="Century Gothic", size=14, weight="bold"),
+    command=Theme,
 )
-Colores.grid(row=0, column=1, padx=20, pady=20)
+Colores.grid(row=0, column=0, padx=20, pady=60)
 Colores.set("Dark")
 
+
+Tab_color.tab("Qrcolor").grid_columnconfigure(0, weight=1)
+
+button_color_fill = kc.CTkButton(
+    Tab_color.tab("Qrcolor"), 
+    text="🎨 COLOR DE RELLENO (FILL)", 
+    command=Color_fill,
+    width=250,
+    height=50,
+    corner_radius=12,
+    fg_color="transparent",
+    border_width=2,
+    border_color="#6bde18",
+    hover_color="#6bde18",
+    text_color="#6bde18",
+    font=kc.CTkFont(family="Century Gothic", size=13, weight="bold")
+)
+button_color_fill.grid(row=0, column=0, pady=(60, 10))
+
+button_color_back = kc.CTkButton(
+    Tab_color.tab("Qrcolor"), 
+    text="🖼️ COLOR DE FONDO (BACK)", 
+    command=Color_back,
+    width=250,
+    height=50,
+    corner_radius=12,
+    fg_color="transparent",
+    border_width=2,
+    border_color="#ffffff",
+    hover_color="#ffffff",
+    text_color="#ffffff",
+    font=kc.CTkFont(family="Century Gothic", size=13, weight="bold")
+)
+button_color_back.grid(row=1, column=0, pady=10)
 App.mainloop()
